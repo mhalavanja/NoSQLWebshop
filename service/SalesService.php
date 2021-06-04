4cc1706b2f08c268d2926f377a34e31ce8869a42
Anđela Konjevod <andela.konjevod@gmail.com>
2021-06-04 20:51:54 +0200

    Dodan getSalesForProduct query. Nije testirano.

--- service/SalesService.php
+++ service/SalesService.php
@@ -3,13 +3,34 @@
 
 class SalesService
 {
-//    static function getSales($reviewId)
-//    {
-//
-//    }
+    //    static function getSales($reviewId)
+    //    {
+    //
+    //    }
 
     static function getSalesForProduct($productId)
     {
-
+        $m = mongoDB::getConnection();
+        $filter = [
+            'saleArray.productId' => $productId
+        ];
+        $options = [
+            'projection' => [
+                'saleArray' => [
+                    '$elemMatch' => [
+                        'productId' => $productId
+                    ]
+                ]
+            ]
+        ];
+        $query = new \MongoDB\Driver\Query($filter, $options);
+        $rows   = $m->executeQuery('projekt.users', $query);
+        foreach ($rows as $document) {
+            $document = json_decode(json_encode($document), true);
+            echo '<pre>';
+            print_r($document);
+            echo '</pre>';
+            echo '<br>';
+        }
     }
 }
