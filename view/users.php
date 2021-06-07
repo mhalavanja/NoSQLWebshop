d3046c4d01ab75d43d69d791ba616aeb5b9c877b
MarinHanic0 <marin.hanic1@gmail.com>
2021-06-07 16:51:12 +0200

    Složen prikaz preporučenih korisnika

--- view/users.php
+++ view/users.php
@@ -1,6 +1,10 @@
 <?php
 require_once __SITE_PATH . '/view/_header.php';
 require_once __SITE_PATH . '/util/userList.php';
-echo "<h1>Korisnici</h1>";
+if(isset($_GET['username'])){
+    echo "<h1>Preporučeni korisnici od ".$_GET['username']."</h1>";
+} else{
+    echo "<h1>Korisnici</h1>";
+}
 if (isset($usernames)) echo getUserList($usernames, true);
 require_once __SITE_PATH . '/view/_footer.php';
